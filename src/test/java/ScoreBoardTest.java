93d64726111810ccd21f055d16acf55547d56e29
cyprian.omenuko <cyprian.omenuko@unite.eu>
2025-05-03 20:02:02 +0100

    test: for creating a Match with zero team score

--- src/test/java/ScoreBoardTest.java
+++ src/test/java/ScoreBoardTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ScoreBoardTest {
+
+    private static final String HOME_TEAM = "Mexico";
+    private static final String AWAY_TEAM = "Canada";
+
+    @Test
+    void shouldCreateNewMatchWithTeamsAndZeroScore() {
+        Match match = new Match(HOME_TEAM, AWAY_TEAM);
+
+        assertEquals(HOME_TEAM, match.getHomeTeam());
+        assertEquals(AWAY_TEAM, match.getAwayTeam());
+        assertEquals(0, match.getHomeScore());
+        assertEquals(0, match.getAwayScore());
+    }
+
 }
