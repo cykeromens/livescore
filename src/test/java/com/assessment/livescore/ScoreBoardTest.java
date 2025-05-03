91bcd082451eb1101c288ad35b62f9c96909d96c
cyprian.omenuko <cyprian.omenuko@unite.eu>
2025-05-03 20:26:02 +0100

    test: verify new Match is displayed on the ScoreBoard

--- src/test/java/com/assessment/livescore/ScoreBoardTest.java
+++ src/test/java/com/assessment/livescore/ScoreBoardTest.java
@@ -2,6 +2,8 @@ package com.assessment.livescore;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScoreBoardTest {
@@ -19,4 +21,18 @@ class ScoreBoardTest {
         assertEquals(0, match.getAwayScore());
     }
 
+    @Test
+    void shouldStartMatchAndDisplayOnScoreBoard() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        List<Match> scoreBoardBeforeStartingMatch = scoreBoard.getSummary();
+        assertEquals(0, scoreBoardBeforeStartingMatch.size());
+
+        scoreBoard.startMatch(HOME_TEAM, AWAY_TEAM);
+
+        List<Match> scoreBoardAfterStartingMatch = scoreBoard.getSummary();
+
+        assertEquals(1, scoreBoardAfterStartingMatch.size());
+        assertEquals(HOME_TEAM, scoreBoardAfterStartingMatch.get(0).getHomeTeam());
+        assertEquals(AWAY_TEAM, scoreBoardAfterStartingMatch.get(0).getAwayTeam());
+    }
 }
